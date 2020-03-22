e2b9cca964c6dc94a51f028f062921591d9c2651
ts3486 <52314726+ts3486@users.noreply.github.com>
2020-03-22 12:12:20 +0900

    Update passion.js

--- passion.js
+++ passion.js
@@ -4,4 +4,16 @@ function Answer() {
 	document.getElementById("two").style.background="red";
 	document.getElementById("three").style.background="red";
 	document.getElementById("four").style.background="red";
+
+	document.getElementById("answer").innerHTML = "Correct!";
+}
+
+function WrongAnswer() {
+
+	document.getElementById("one").style.background="#CEFF33";
+	document.getElementById("two").style.background="red";
+	document.getElementById("three").style.background="red";
+	document.getElementById("four").style.background="red";
+
+	document.getElementById("answer").innerHTML = "Incorrect!";
 }
