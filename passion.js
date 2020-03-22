5dd9af7328134473feca67826a38a8e9ca945e56
ts3486 <52314726+ts3486@users.noreply.github.com>
2020-03-22 11:01:39 +0900

    Add files via upload

---
+++ passion.js
@@ -0,0 +1,7 @@
+function Answer() {
+
+	document.getElementById("one").style.background="#CEFF33";
+	document.getElementById("two").style.background="red";
+	document.getElementById("three").style.background="red";
+	document.getElementById("four").style.background="red";
+}
